81e6eb1d78b24543e28dc132968d5070970e121b
DMJC <jcarthew@gmail.com>
2025-08-28 02:16:17 +1000

    Initial import

---
+++ FSNode/config.h
@@ -0,0 +1,77 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* debug logging */
+#define GW_DEBUG_LOG 0
+
+/* Define to 1 if you have the `getmntent' function. */
+#define HAVE_GETMNTENT 1
+
+/* Define to 1 if you have the `getmntinfo' function. */
+/* #undef HAVE_GETMNTINFO */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <mntent.h> header file. */
+#define HAVE_MNTENT_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/mntent.h> header file. */
+/* #undef HAVE_SYS_MNTENT_H */
+
+/* Define to 1 if you have the <sys/statvfs.h> header file. */
+#define HAVE_SYS_STATVFS_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* mntent structure member name */
+#define MNT_DIR mnt_dir
+
+/* mntent structure member name */
+#define MNT_FSNAME mnt_fsname
+
+/* mntent structure member name */
+#define MNT_FSTYPE mnt_type
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
---
+++ Tools/wopen/wopen.m
@@ -0,0 +1,86 @@
+/*
+ *  wopen.m: Implementation of the wopen tool 
+ *  for the GNUstep GFinder application
+ *
+ *  Copyright (C) 2002-2025 Free Software Foundation, Inc.
+ *
+ *   Author: Enrico Sersale
+ *   Date: September 2002
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 31 Milk Street #960789 Boston, MA 02196 USA.
+ */
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+int main(int argc, char** argv, char **env_c)
+{
+  NSAutoreleasePool *pool;
+  NSArray *arguments = nil;
+  NSFileManager *fm = nil;
+  NSString *basePath = nil;
+  NSString *fpath = nil;
+  NSString *fullPath = nil;
+  BOOL isDir = NO;
+  id gfinder = nil;
+   
+  pool = [NSAutoreleasePool new];
+  fm = [NSFileManager defaultManager];
+  
+  if (argc < 2)
+    {
+      NSLog(@"no arguments supplied. exiting now.");
+      [pool release];
+      exit(0);
+    }
+  else
+    {
+      basePath = [fm currentDirectoryPath];
+      arguments = [[NSProcessInfo processInfo] arguments];
+      fpath = [arguments objectAtIndex: 1];
+        
+      if ([fpath isAbsolutePath] && [fm fileExistsAtPath: fpath isDirectory: &isDir])
+        {
+          fullPath = fpath;
+        }
+      else
+        {
+          fullPath = [basePath stringByAppendingPathComponent: fpath];
+          fullPath = [fullPath stringByStandardizingPath];
+
+          if ([fm fileExistsAtPath: fullPath isDirectory: &isDir] == NO)
+            {
+              NSLog(@"%@ doesn't exist. exiting now.", fpath);
+              [pool release];
+              exit(0);
+            }
+        }
+
+      gfinder = [NSConnection rootProxyForConnectionWithRegisteredName: @"GFinder"
+                                                                     host: @""];
+      if (gfinder == nil)
+        {
+          NSLog(@"can't contact GFinder via %@. exiting now.", fpath);
+          [pool release];
+          exit(0);
+        }
+
+      [gfinder application: gfinder openFile: fullPath];
+    }
+
+  [pool release];
+  exit(0);
+}
